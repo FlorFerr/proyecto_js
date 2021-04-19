54cada30bf84c65e7224afbcbca13066568c89c8
FlorFerr <florferreyra16@gmail.com>
2021-04-19 19:47:14 -0300

    Agreguar archivo JSON con los productos

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
+//Pasar los datos del JSON al DOM
+const URLJSON = "js/data.json"
+
+function agregarProductos(){
+$.getJSON(URLJSON, function (respuesta, estado) {
+    if(estado === "success"){
+      let productos = respuesta;
+      for (const item of productos) {
+        $(".productos__contenedor").append(`<div class='col'>         
+        <div class='card'>
+          <h3 class='card-title'>${item.nombre}</h3>
+          <div class='card-body'>
+            <img src='${item.foto}' class='card-img-top'/>
+          </div>
+          <p class ='card-subtitle'>$${item.precio}</p>
+        </div>
+        <button class="btn btn-info botonCompra" id="${item.nombre}">Agregar al carrito</button>
+      </div>`)
+      }  
+    //Seleccionar el botón para el evento 
+    let botones = $(".botonCompra") 
+   
+          for (boton of botones){
+              $(boton).click(function(e){
+                
+                item = productos.filter(producto => producto.nombre === e.target.id)[0] //Filtro por ID
+                //EL CERO ENTRE CORCHETES ES PARA QUE ELIJA EL PRIMER Y UNICO PRODUCTO
+                console.log(item);
+                listaOrden.push({nombre: item.nombre, precio: item.precio});
+                agregarCarrito();
+    
+    })}
+    }
+    })}
+
 //Array para los productos seleccionados
 let listaOrden = [];
-
-//Función para pasar el array de productos al HTML
-function agregarProductos(){
-    for (let item of arrayProductos) {
-
-        $(".productos__contenedor").append(
-          `<div class='col'>         
-            <div class='card'>
-              <h3 class='card-title'>${item.nombre}</h3>
-              <div class='card-body'>
-                <img src='${item.foto}' class='card-img-top'/>
-              </div>
-              <p class ='card-subtitle'>$${item.precio}</p>
-            </div>
-            <button class="btn btn-info botonCompra" id="${item.nombre}">Agregar</button>
-          </div>`
-        )}
-    //Seleccionar el botón para el evento
-    let botones = $(".botonCompra") 
-      //HACEMOS OTRO FOR PARA ESCUCHAR LOS EVENTOS, SINO HACIAS MUCHISIMOS EVENTOS DE SOBRA
-      for (boton of botones){
-          $(boton).click(function(e){//E SERIA EL EVENTO QUE CAPTA
-            console.log(e.target.id) //SELECCIONO EL ID QUE TIENE LA DESCRIPCION DEL PRODUCTO CORRESPONDIENTE
-            item = arrayProductos.filter(producto => producto.nombre === e.target.id)[0] //Filtro por ID
-            //EL CERO ENTRE CORCHETES ES PARA QUE ELIJA EL PRIMER Y UNICO PRODUCTO
-            console.log(item);
-            listaOrden.push({nombre: item.nombre, precio: item.precio});
-            agregarCarrito();
-
-})}}
-            
 //Función para mostrar los productos seleccionados
 function agregarCarrito(){
     let orden = [];
@@ -40,7 +44,7 @@ function agregarCarrito(){
                   <td>$${producto['precio']}</td>
                   </tr>`
     }
-    //Selecciono el elemento de l table a donde quiero que se asignen los nuevos elementos
+    //Selecciono el elemento del table a donde quiero que se asignen los nuevos elementos
     let tabla = document.querySelector("tbody")
     tabla.innerHTML = (orden)
     //Nuevo array con los precios de los productos seleccionados
@@ -67,9 +71,9 @@ $(".btnQuitar").click(function(){
 //Llamada a la función
 agregarProductos()
 
-
+//Animación
 $(".container").prepend('<p id="envios">¡Envíos gratis en compras mayores a $5000!</p>');
-//Declaración de métodos encadenados
+
 $("#envios").css("display", "none")
         .css("font-size","20px")
         .css("margin-top","20px")
